29831a28f4109f9c92515832c110548e6dff3f2a
Grace Ariel <madyagra@gmail.com>
2024-04-28 22:18:42 +0800

    rename env variables

--- database/connect.go
+++ database/connect.go
@@ -18,12 +18,13 @@ func ConnectDB() {
 	}
 
 	db, err = sql.Open("postgres",
-		fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s %s",
 			config.Config("DB_HOST"),
 			port,
-			config.Config("DB_USER"),
+			config.Config("DB_USERNAME"),
 			config.Config("DB_PASSWORD"),
 			config.Config("DB_NAME"),
+			config.Config("DB_PARAMS"),
 		))
 	if err != nil {
 		log.Error(err.Error())
--- handler/auth.go
+++ handler/auth.go
@@ -108,7 +108,7 @@ func Login(c *fiber.Ctx) error {
 	claims["user_id"] = ud.ID
 	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
 
-	t, err := token.SignedString([]byte(config.Config("SECRET")))
+	t, err := token.SignedString([]byte(config.Config("JWT_SECRET")))
 	if err != nil {
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
