c82f7ccbcf06095b8b11de7bf60a4bb25ff6c270
Grace Ariel <madyagra@gmail.com>
2024-05-01 17:34:29 +0800

    add cat matches migration files

---
+++ migrations/20240501093015_create_cat_matches.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS cat_matches(
+  id SERIAL PRIMARY KEY,
+  message VARCHAR(255),
+  issuer_cat_id INT references cats(id) NOT NULL,
+  receiver_cat_id INT references cats(id) NOT NULL,
+  status VARCHAR(100) NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
